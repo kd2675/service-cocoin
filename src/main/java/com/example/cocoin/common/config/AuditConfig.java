713814939511c7803697e20e6d01b14c43d1eae4
김도영 <zz01064842675@gmail.com>
2024-04-15 13:30:59 +0900

    init

--- src/main/java/com/example/cocoin/common/config/AuditConfig.java
+++ src/main/java/com/example/cocoin/common/config/AuditConfig.java
@@ -4,10 +4,23 @@ import com.example.cocoin.common.config.jwt.provider.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
-public class AuditConfig {
+@EnableJpaAuditing // JPA Auditing 활성화
+public class AuditConfig implements AuditorAware<String> {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        try {
+            String currentUserEmail = jwtTokenProvider.getUserEmail();
+            return Optional.of(currentUserEmail);
+        } catch (ClassCastException e) {
+            return Optional.empty();
+        }
+    }
 }
--- src/main/java/com/example/cocoin/common/config/JpaConfig.java
+++ src/main/java/com/example/cocoin/common/config/JpaConfig.java
@@ -1,41 +1,27 @@
 package com.example.cocoin.common.config;
 
-import com.example.cocoin.common.config.jwt.provider.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
-import java.util.Optional;
 
 @Configuration
-@EnableJpaAuditing // JPA Auditing 활성화
-@RequiredArgsConstructor
-public class JpaConfig implements AuditorAware<String> {
-    private final JwtTokenProvider jwtTokenProvider;
-
+public class JpaConfig {
     @Bean
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
     }
     @Bean
     public EntityManagerFactoryBuilder entityManagerFactoryBuilder() {
-        return new EntityManagerFactoryBuilder(new HibernateJpaVendorAdapter(), new HashMap<>(), null);
-    }
-
-    @Override
-    public Optional<String> getCurrentAuditor() {
-        try {
-            String currentUserEmail = jwtTokenProvider.getUserEmail();
-            return Optional.of(currentUserEmail);
-        } catch (ClassCastException e) {
-            return Optional.empty();
-        }
+        return new EntityManagerFactoryBuilder(
+                new HibernateJpaVendorAdapter(),
+                new HashMap<>(),
+                null
+        );
     }
 }
