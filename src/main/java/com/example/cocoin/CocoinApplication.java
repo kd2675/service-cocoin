3477638e0f76f85dcd1002dd72339eaf08b62e69
김도영 <zz01064842675@gmail.com>
2024-04-24 18:00:39 +0900

    module 진행중 market 체결 로그 작성중 build 파일도 정리해야됨

--- src/main/java/com/example/cocoin/CocoinApplication.java
+++ src/main/java/com/example/cocoin/CocoinApplication.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CocoinApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CocoinApplication.class, args);
-    }
 
+        String getVersion = org.springframework.core.SpringVersion.getVersion();
+        System.out.println("SpringVersion =============> " + getVersion);
+    }
 }
